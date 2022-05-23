fadad64610aa92c4271163aff10cf8caaf577ba7
Thalles <thallesmx5_@hotmail.com>
2022-05-23 11:56:18 -0300

    criação do projeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Componente de estado modificado //
+class App extends React.Component{
+
+  state = {
+    nome : ''
+  }
+
+  modificarNome  = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  render(){
+    return(
+      <>
+      <input type="text" value={this.state.nome} onChange={this.modificarNome}/>
+      <h1>
+        hello {this.state.nome}
+      </h1>
+      </>
+    )
+  }
 }
+
 
 export default App;
